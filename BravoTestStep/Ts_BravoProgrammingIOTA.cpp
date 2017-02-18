2d6ff465087e315b1e7621995119af427762ae58
JerryZhang <JerryZhang@cn.synaptics.com>
2017-02-18 09:15:32 +0800

    add write wof iota

--- BravoTestStep/Ts_BravoProgrammingIOTA.cpp
+++ BravoTestStep/Ts_BravoProgrammingIOTA.cpp
@@ -52,6 +52,18 @@ void Ts_BravoProgrammingIOTA::Execute()
 	uint32_t rc = 0;
 	Syn_Exception Exception(0);
 
+	//add bincode check, todo......
+
+
+
+
+
+
+
+
+
+
+
 	Syn_Patch_Info IOTAWritePatch;
 	if (!_pSynDutUtils->Config_MT_Info.GetPatchInfo("iotawrite", IOTAWritePatch))
 	{
@@ -188,49 +200,150 @@ void Ts_BravoProgrammingIOTA::Execute()
 
 		//FW_BL and LNA_BL
 		CalibrateTestData *pCalibrateData = static_cast<CalibrateTestData*>(RetrieveTestData("Calibrate"));
-		if (NULL == pCalibrateData)
+		if (NULL != pCalibrateData)
 		{
-			Exception.SetError(ERROR_TSETSTEP_UNEXCUTED);
-			Exception.SetDescription("BravoProgrammingIOTA::RetrieveTestData(Calibrate) is failed!");
-			throw Exception;
-			return;
+			//add tag : fw_bl && lna_bl 
+			vcsfw_frame_tag_t FrameBase_FW_BL_tag;
+			FrameBase_FW_BL_tag.nwords = (rowNumber * columnNumber * 2) / sizeof(uint32_t);//number of 32-bit to follow
+			FrameBase_FW_BL_tag.flags = 0x20;
+			FrameBase_FW_BL_tag.tagid = VCSFW_FRAME_TAG_FW_BL;
+			uint8_t *arr_FW_BL = new uint8_t[rowNumber * columnNumber * 2 + sizeof(vcsfw_frame_tag_t)];
+			memcpy(arr_FW_BL, &FrameBase_FW_BL_tag, sizeof(vcsfw_frame_tag_t));
+			memcpy(&(arr_FW_BL[sizeof(vcsfw_frame_tag_t)]), &(pCalibrateData->FWBaseline), rowNumber*columnNumber * 2);
+			rc = _pSynModule->FpIotawrite(arr_FW_BL, rowNumber * columnNumber * 2 + sizeof(vcsfw_frame_tag_t), VCSFW_IOTA_ITYPE_FRAME_BASE);
+			delete[] arr_FW_BL;
+			arr_FW_BL = NULL;
+			if (0 != rc)
+			{
+				Exception.SetError(rc);
+				Exception.SetDescription("BravoProgrammingIOTA::FpIotawrite(FW_BL) is failed!");
+				throw Exception;
+				return;
+			}
+
+			vcsfw_frame_tag_t FrameBase_LNA_BL_tag;
+			FrameBase_LNA_BL_tag.nwords = (rowNumber * columnNumber * 2) / sizeof(uint32_t);//number of 32-bit to follow
+			FrameBase_LNA_BL_tag.flags = 0x20;
+			FrameBase_LNA_BL_tag.tagid = VCSFW_FRAME_TAG_LNA_BL;
+			uint8_t *arr_LNA_BL = new uint8_t[rowNumber*columnNumber * 2 + sizeof(vcsfw_frame_tag_t)];
+			memcpy(arr_LNA_BL, &FrameBase_LNA_BL_tag, sizeof(vcsfw_frame_tag_t));
+			memcpy(&(arr_LNA_BL[sizeof(vcsfw_frame_tag_t)]), &(pCalibrateData->LNABaseline), rowNumber * columnNumber * 2);
+			rc = _pSynModule->FpIotawrite(arr_LNA_BL, rowNumber * columnNumber * 2 + sizeof(vcsfw_frame_tag_t), VCSFW_IOTA_ITYPE_FRAME_BASE);
+			delete[] arr_LNA_BL;
+			arr_LNA_BL = NULL;
+			if (0 != rc)
+			{
+				Exception.SetError(rc);
+				Exception.SetDescription("BravoProgrammingIOTA::FpIotawrite(LNA_BL) is failed!");
+				throw Exception;
+				return;
+			}
 		}
 
-		//add tag : fw_bl && lna_bl 
-		vcsfw_frame_tag_t FrameBase_FW_BL_tag;
-		FrameBase_FW_BL_tag.nwords = (rowNumber * columnNumber * 2) / sizeof(uint32_t);//number of 32-bit to follow
-		FrameBase_FW_BL_tag.flags = 0x20;
-		FrameBase_FW_BL_tag.tagid = VCSFW_FRAME_TAG_FW_BL;
-		uint8_t *arr_FW_BL = new uint8_t[rowNumber * columnNumber * 2 + sizeof(vcsfw_frame_tag_t)];
-		memcpy(arr_FW_BL, &FrameBase_FW_BL_tag, sizeof(vcsfw_frame_tag_t));
-		memcpy(&(arr_FW_BL[sizeof(vcsfw_frame_tag_t)]), &(pCalibrateData->FWBaseline), rowNumber*columnNumber * 2);
-		rc = _pSynModule->FpIotawrite(arr_FW_BL, rowNumber * columnNumber * 2 + sizeof(vcsfw_frame_tag_t), VCSFW_IOTA_ITYPE_FRAME_BASE);
-		delete[] arr_FW_BL;
-		arr_FW_BL = NULL;
-		if (0 != rc)
+		//WOF
+		WOF_BaselineTestData *pWOF_BaselineTestData = static_cast<WOF_BaselineTestData*>(RetrieveTestData("WOF_Baseline"));
+		WOF_SignalTestData *pWOF_SignalTestData = static_cast<WOF_SignalTestData*>(RetrieveTestData("WOF_Signal"));
+		if (NULL != pWOF_BaselineTestData&&NULL != pWOF_SignalTestData)
 		{
-			Exception.SetError(rc);
-			Exception.SetDescription("BravoProgrammingIOTA::FpIotawrite(FW_BL) is failed!");
-			throw Exception;
-			return;
-		}
+			uint32_t gainValue = pWOF_SignalTestData->selectedGain;
+			uint16_t offsetValue = pWOF_SignalTestData->selectedOffset;
+			uint32_t signalValue = pWOF_SignalTestData->selectedSingal;
 
-		vcsfw_frame_tag_t FrameBase_LNA_BL_tag;
-		FrameBase_LNA_BL_tag.nwords = (rowNumber * columnNumber * 2) / sizeof(uint32_t);//number of 32-bit to follow
-		FrameBase_LNA_BL_tag.flags = 0x20;
-		FrameBase_LNA_BL_tag.tagid = VCSFW_FRAME_TAG_LNA_BL;
-		uint8_t *arr_LNA_BL = new uint8_t[rowNumber*columnNumber * 2 + sizeof(vcsfw_frame_tag_t)];
-		memcpy(arr_LNA_BL, &FrameBase_LNA_BL_tag, sizeof(vcsfw_frame_tag_t));
-		memcpy(&(arr_LNA_BL[sizeof(vcsfw_frame_tag_t)]), &(pCalibrateData->LNABaseline), rowNumber * columnNumber * 2);
-		rc = _pSynModule->FpIotawrite(arr_LNA_BL, rowNumber * columnNumber * 2 + sizeof(vcsfw_frame_tag_t), VCSFW_IOTA_ITYPE_FRAME_BASE);
-		delete[] arr_LNA_BL;
-		arr_LNA_BL = NULL;
-		if (0 != rc)
-		{
-			Exception.SetError(rc);
-			Exception.SetDescription("BravoProgrammingIOTA::FpIotawrite(LNA_BL) is failed!");
-			throw Exception;
-			return;
+			//REG16BLK
+			vcsfw_frame_tag_t frame_tag_gain;
+			frame_tag_gain.tagid = VCSFW_FRAME_TAG_REG16BLK;
+			frame_tag_gain.nwords = 2;
+			frame_tag_gain.flags = 0x20;
+
+			vcsfw_frame_tag_reg16blk_t frame_tag_reg16blk_gain;
+			frame_tag_reg16blk_gain.regbase = 0x4950;//0x4950 is for shasta, denali is 0x2658
+			frame_tag_reg16blk_gain.nregs = 1;
+
+			uint8_t *arrGain = new uint8_t[sizeof(vcsfw_frame_tag_t)+sizeof(vcsfw_frame_tag_reg16blk_t)+sizeof(uint32_t)];
+			memcpy(&(arrGain[0]), &frame_tag_gain, sizeof(vcsfw_frame_tag_t));
+			memcpy(&(arrGain[sizeof(vcsfw_frame_tag_t)]), &frame_tag_reg16blk_gain, sizeof(vcsfw_frame_tag_reg16blk_t));
+			memcpy(&(arrGain[sizeof(vcsfw_frame_tag_t)+sizeof(vcsfw_frame_tag_reg16blk_t)]), &gainValue, sizeof(uint32_t));
+			rc = _pSynModule->FpIotawrite(arrGain, sizeof(vcsfw_frame_tag_t)+sizeof(vcsfw_frame_tag_reg16blk_t)+sizeof(uint32_t), VCSFW_IOTA_ITYPE_FRAME_BASE);
+			delete[] arrGain;
+			arrGain = NULL;
+			if (0 != rc)
+			{
+				Exception.SetError(rc);
+				Exception.SetDescription("BravoProgrammingIOTA::FpIotawrite(gain) is failed!");
+				throw Exception;
+				return;
+			}
+			
+			//REG32BLK
+			vcsfw_frame_tag_t frame_tag_offset;
+			frame_tag_offset.tagid = VCSFW_FRAME_TAG_REG32BLK;
+			frame_tag_offset.nwords = 2;
+			frame_tag_offset.flags = 0x20;
+
+			vcsfw_frame_tag_reg32blk_t frame_tag_reg32blk_gain;
+			frame_tag_reg32blk_gain.regbase = 0x039c;
+			frame_tag_reg32blk_gain.nregs = 1;
+
+			uint32_t offsetValue32bits = 0;
+			offsetValue32bits = (offsetValue << 12) + offsetValue;
+
+			uint8_t *arrOffset = new uint8_t[sizeof(vcsfw_frame_tag_t)+sizeof(vcsfw_frame_tag_reg32blk_t)+sizeof(uint32_t)];
+			memcpy(&(arrOffset[0]), &frame_tag_offset, sizeof(vcsfw_frame_tag_t));
+			memcpy(&(arrOffset[sizeof(vcsfw_frame_tag_t)]), &frame_tag_reg32blk_gain, sizeof(vcsfw_frame_tag_reg32blk_t));
+			memcpy(&(arrOffset[sizeof(vcsfw_frame_tag_t)+sizeof(vcsfw_frame_tag_reg32blk_t)]), &offsetValue32bits, sizeof(uint32_t));
+			rc = _pSynModule->FpIotawrite(arrOffset, sizeof(vcsfw_frame_tag_t)+sizeof(vcsfw_frame_tag_reg32blk_t)+sizeof(uint32_t), VCSFW_IOTA_ITYPE_FRAME_BASE);
+			delete[] arrOffset;
+			arrOffset = NULL;
+			if (0 != rc)
+			{
+				Exception.SetError(rc);
+				Exception.SetDescription("BravoProgrammingIOTA::FpIotawrite(offset) is failed!");
+				throw Exception;
+				return;
+			}
+
+			//CONFIG_WOF_THRESHOLDS
+			vcsfw_config_wof_thresholds_t config_wof_thresholds;
+			config_wof_thresholds.signal = signalValue;
+			config_wof_thresholds.hyst = 5;
+			config_wof_thresholds.reduction = 12;
+			rc = _pSynModule->FpIotawrite((uint8_t*)&config_wof_thresholds, sizeof(vcsfw_config_wof_thresholds_t), VCSFW_IOTA_ITYPE_CONFIG_WOF_THRESHOLDS);
+			if (0 != rc)
+			{
+				Exception.SetError(rc);
+				Exception.SetDescription("BravoProgrammingIOTA::FpIotawrite(CONFIG_WOF_THRESHOLDS) is failed!");
+				throw Exception;
+				return;
+			}
+
+			//REG32BLK
+			vcsfw_frame_tag_t frame_tag_ctrl;
+			frame_tag_ctrl.tagid = VCSFW_FRAME_TAG_REG32BLK;
+			frame_tag_ctrl.nwords = 2;
+			frame_tag_ctrl.flags = 0x20;
+
+			vcsfw_frame_tag_reg32blk_t frame_tag_reg32blk_ctrl;
+			frame_tag_reg32blk_ctrl.regbase = 0x0384;
+			frame_tag_reg32blk_ctrl.nregs = 1;
+
+			uint32_t ctrlValue32bits = 0;
+			uint32_t FD_ACCUM = 0, FU_ACCUM = 0, FD_SAMPLES = 1, FU_SAMPLES = 1, FH_SAMPLES = 1;
+			ctrlValue32bits = (FD_SAMPLES << 2) + (FU_SAMPLES << 12) + (FH_SAMPLES << 22);
+
+			uint8_t *arrCtrl = new uint8_t[sizeof(vcsfw_frame_tag_t)+sizeof(vcsfw_frame_tag_reg32blk_t)+sizeof(uint32_t)];
+			memcpy(&(arrCtrl[0]), &frame_tag_offset, sizeof(vcsfw_frame_tag_t));
+			memcpy(&(arrCtrl[sizeof(vcsfw_frame_tag_t)]), &frame_tag_reg32blk_ctrl, sizeof(vcsfw_frame_tag_reg32blk_t));
+			memcpy(&(arrCtrl[sizeof(vcsfw_frame_tag_t)+sizeof(vcsfw_frame_tag_reg32blk_t)]), &ctrlValue32bits, sizeof(uint32_t));
+			rc = _pSynModule->FpIotawrite(arrCtrl, sizeof(vcsfw_frame_tag_t)+sizeof(vcsfw_frame_tag_reg32blk_t)+sizeof(uint32_t), VCSFW_IOTA_ITYPE_FRAME_BASE);
+			delete[] arrCtrl;
+			arrCtrl = NULL;
+			if (0 != rc)
+			{
+				Exception.SetError(rc);
+				Exception.SetDescription("BravoProgrammingIOTA::FpIotawrite(ctrl) is failed!");
+				throw Exception;
+				return;
+			}
 		}
 	}
 
